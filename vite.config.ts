67979969d6d235b6ddaf4f791d1f95bc10cf7014
Cicada <1134022833@qq.com>
2025-11-29 02:27:27 +0800

    style: 部署路径修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/canvas_platform/', // GitHub Pages 部署路径，改成你的仓库名
+  base: '/canvas-platform/', // GitHub Pages 部署路径
 })
